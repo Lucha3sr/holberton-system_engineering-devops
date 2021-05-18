efb1edeb087b79c6070da9ac5a97c5c8f86e6ad7
Lucha3sr <2278@holbertonschool.com>
2021-05-18 19:10:48 +0000

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """If an invalid subreddit is given, the function should return 0"""
-    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     r = requests.get("https://www.reddit.com/r/{}/about.json"
-                     .format(subreddit))
+                     .format(subreddit), headers=headers)
     if r.status_code == 404:
         return 0
     else:
