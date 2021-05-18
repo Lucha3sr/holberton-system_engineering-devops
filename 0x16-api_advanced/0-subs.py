b1b28736dc12dbfb13eef5918c1e04d9a68068bd
Lucha3sr <2278@holbertonschool.com>
2021-05-18 19:02:17 +0000

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-function that queries the Reddit API and returns the number of subscribers for a given subreddit
-"""
+"""function that queries the Reddit API and returns the nbr of subscribers"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     """If an invalid subreddit is given, the function should return 0"""
-    #headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
+    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     r = requests.get("https://www.reddit.com/r/{}/.json".format(subreddit))
     if r.status_code == 404:
         return 0
@@ -14,4 +13,3 @@ def number_of_subscribers(subreddit):
         r = r.json()
         subscribers = r.get('subreddit_subscribers')
         return subscribers
-
