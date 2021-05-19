8f246dea26559abee9e22d2243f21b878df7b302
Lucha3sr <2278@holbertonschool.com>
2021-05-19 18:27:31 +0000

    Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# fix wordpress 500 error
+# Using strace, find out why Apache is returning a 500 error
 exec { 'replace_line':
   command => 'sed -i s/phpp/php /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
