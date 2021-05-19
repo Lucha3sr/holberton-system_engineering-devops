f8af8077d6460fe4283d610bab4773744c80c971
Lucha3sr <2278@holbertonschool.com>
2021-05-19 02:47:21 +0000

    api advanced

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """If no results are found the function should return None"""
