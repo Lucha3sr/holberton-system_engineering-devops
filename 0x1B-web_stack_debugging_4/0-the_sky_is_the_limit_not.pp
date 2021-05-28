b45767d46fe8e1812347cd5be5caa41eb70165c3
Lucha3sr <2278@holbertonschool.com>
2021-05-28 00:53:28 +0000

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #fix Nginx requests failed
 exec { 'limit_requests_increase':
-command => 'sed -i "s/15/30000/g" /etc/default/nginx; sudo service nginx restart',
+command => 'sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
 path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
